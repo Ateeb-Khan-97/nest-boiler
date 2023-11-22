10083a962e98e76ac50f8bc9e0fd2882deb79fec
Ateeb Khan <ateeb@fynder.ai>
2023-11-22 05:25:08 +0500

    initial commit

--- src/modules/auth/auth.user.decorator.ts
+++ src/modules/auth/auth.user.decorator.ts
@@ -1,23 +1,15 @@
 import { ExecutionContext, createParamDecorator } from '@nestjs/common';
-import { IsString } from 'class-validator';
 
-export const User = createParamDecorator(
+export const CurrentUser = createParamDecorator(
   (data: unknown, ctx: ExecutionContext): IUser => {
     const request = ctx.switchToHttp().getRequest();
     return request.user as IUser;
   },
 );
 
-export abstract class IUser {
-  @IsString()
-  public id: string;
-
-  @IsString()
-  public name: string;
-
-  @IsString()
-  public username: string;
-
-  @IsString()
-  public email: string;
+export interface IUser {
+  id: string;
+  name: string;
+  username: string;
+  email: string;
 }
